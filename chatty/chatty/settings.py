4ac6352964960fcb882ccfaf22bd0af431975ade
David <dmiller89@gmail.com>
2020-08-04 15:38:24 -0600

    Adding cors middleware

--- chatty/chatty/settings.py
+++ chatty/chatty/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8s3!p@2bx6cbgtym!f-^7r)y!@ut9wv*b@m*+(4m$qqj#^+o7^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'chat',
     'channels',
 ]
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'chatty.urls'
